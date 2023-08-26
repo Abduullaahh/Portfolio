8811ff6908f22288955179661fd4ee69d51aa9ca
Abdullah Tahir <108390173+Abduullaahh@users.noreply.github.com>
2023-08-27 02:04:50 +0500

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'jade');
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://muhammad-abdullah-tahir.netlify.app/");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
